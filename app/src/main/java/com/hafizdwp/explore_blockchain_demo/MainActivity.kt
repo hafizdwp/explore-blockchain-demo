3a9f761806194658bb6d11ece4c570f2e52e8625
hafizdwp <hafizdwp@gmail.com>
2021-05-10 16:54:22 +0700

    Initial commit

---
+++ app/src/main/java/com/hafizdwp/explore_blockchain_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hafizdwp.explore_blockchain_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
